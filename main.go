52c0eb22ae0d66d7b3d762baffaa121532e93b6c
jijj <Chinese10920>
2022-06-17 16:41:18 +0800

    feat:gin

--- main.go
+++ main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/goproxy/goproxy"
+	"net/http"
 	"os"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", gin.WrapH(&goproxy.Goproxy{
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = http.ListenAndServe("localhost:8138", &goproxy.Goproxy{
 		GoBinEnv: append(
 			os.Environ(),
 			"GOPROXY=https://goproxy.cn,direct", // 使用 Goproxy.cn 作为上游代理
+			"GOPRIVATE=git.example.com",         // 解决私有模块的拉取问题（比如可以配置成公司内部的代码源）
 		),
 		ProxiedSUMDBs: []string{
 			"sum.golang.org https://goproxy.cn/sumdb/sum.golang.org", // 代理默认的校验和数据库
 		},
-	}))
-	_ = r.Run(":8138")
+	})
 }
